6ea1da93aeceb46b3d9374d71bd66078bdaceb99
Vitaly Rymarev <v.rymarev@a1qa.com>
2016-06-03 15:18:07 +0300

    Packet add

--- RegExpIP/Program.cs
+++ RegExpIP/Program.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
+
 
 namespace RegExpIP
 {
+    // Define a class to receive parsed values
+    class Options
+    {
+        [Option('f', "file", Required = true,
+          HelpText = "Input file to be processed.")]
+        public string InputFile { get; set; }
+
+        [Option('r', "rows", DefaultValue = 10, Required = false,
+          HelpText = "Number of rows")]
+        public int Rows { get; set; }
+
+    }
+
     class Program
     {
+
+        string 
         static void Main(string[] args)
         {
         }
