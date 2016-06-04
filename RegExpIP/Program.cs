94d0acdd103cc4f9edd2044c3759e8cbe7774175
Vitaly Rymarev <v.rymarev@a1qa.com>
2016-06-04 17:36:53 +0300

    file open fix1

--- RegExpIP/Program.cs
+++ RegExpIP/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using CommandLine;
-
+using System.IO;
 
 namespace RegExpIP
 {
@@ -40,34 +40,48 @@ namespace RegExpIP
                 String argStr;
                 argStr = System.Console.ReadLine();
                 if (argStr == "")
-                {
+                { //if empty string - take hardcoded filename
                     options.InputFile = "TestData_IP4RegExp_DataSet1.txt";
                     options.OutputFile = "TestData_IP4RegExp_DataSet1_result.txt";
                 }
                 else if (argStr.ToLower() == "EXIT")
-                {
+                { //EXIT
                     Console.WriteLine("Bye");
                     return 1;
                 }
                 else
                 {
-                    // Instantiate the regular expression object.
+                    // regex for filename
                     string patFileName = @"(.+?)(\.[^.]*$|$)";
                     Regex r1 = new Regex(patFileName, RegexOptions.IgnoreCase);
 
+                    // regex for file extension
                     string patFileExt = @"\.([A-Za-z0-9]+)$";
                     Regex r2 = new Regex(patFileName, RegexOptions.IgnoreCase);
 
                     Match filename1 = r1.Match(argStr);
                     Match filename2 = r2.Match(argStr);
 
+                    //input file
                     options.InputFile = argStr;
+                    //output file
                     options.OutputFile = String.Concat(filename1.ToString(),"_result.",filename2.ToString());
                 }
-
-
-
             }
+
+            FileStream fs = null;
+
+            try
+            {
+                fs = new FileStream(options.InputFile, FileMode.Open);
+            }
+            catch
+            {
+                System.Console.WriteLine("Imposible to open file ");
+                return 1;
+            }
+            
+
 
 
 
